e9851640c0953f08f3a21738dbde9c4e277a3b84
alejandropm-san <x267148@servexternos.gruposantander.es>
2023-04-27 10:29:52 +0200

    All-in-one controller with more info

--- src/main/java/com/example/dynamicro/web/DynaDemoController.java
+++ src/main/java/com/example/dynamicro/web/DynaDemoController.java
@@ -1,22 +1,27 @@
 package com.example.dynamicro.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.tracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/demo")
 @Slf4j
 public class DynaDemoController {
 
@@ -31,43 +36,51 @@ public class DynaDemoController {
 
     RestTemplate restTemplate;
 
+    RestTemplateBuilder restTemplateBuilder;
+
     WebClient webClient;
 
-    public DynaDemoController(RestTemplateBuilder restTemplateBuilder, WebClient.Builder webClientBuilder) {
-        final String URI = "http://localhost:8080";
-        this.restTemplate = restTemplateBuilder.rootUri(URI).build();
-        this.webClient = webClientBuilder.baseUrl(URI).build();
+    ObjectMapper objectMapper;
+
+    public DynaDemoController(RestTemplateBuilder restTemplateBuilder, WebClient.Builder webClientBuilder,
+                              ObjectMapper objectMapper, @Value("${myURL:http://localhost:8080}") String url) {
+        this.restTemplateBuilder = restTemplateBuilder;
+        this.restTemplate = restTemplateBuilder.rootUri(url).build();
+        this.webClient = webClientBuilder.baseUrl(url).build();
+        this.objectMapper = objectMapper;
     }
 
-    @GetMapping(value = "/headers")
-    public void headers(@RequestHeader HttpHeaders headers) {
-        var traceParent = headers.get("traceparent");
-        var traceState = headers.get("tracestate");
+    @GetMapping(value = "/demo")
+    public void headers(@RequestHeader HttpHeaders headers, @RequestParam(name = "internal", required = false) boolean internalSpan,
+                        @RequestParam(required = false) boolean propagation, @RequestParam(required = false) String url) throws JsonProcessingException {
+        Map<String, Object> logData = new HashMap<>();
+        logData.put("control", "FIRST");
+        logData.put("headers", objectMapper.writeValueAsString(headers));
+        logData.put("MDC", objectMapper.writeValueAsString(MDC.getCopyOfContextMap()));
         var context = microTracer.currentSpan().context();
-        log.info("HEADERS(traceParent: {}, traceState:{}) - TRACER(traceId: {}, spanId: {}, parentId: {})",
-                traceParent, traceState, context.traceId(), context.spanId(), context.parentId());
-    }
+        logData.put("tracer", "{ \"traceId\": \"" + context.traceId() + "\", \"spanId\": \"" + context.spanId() + "\", \"parentId\": \"" + context.parentId() + "\"}");
+        log.info(objectMapper.writeValueAsString(logData).replace("\\\"", "\"")
+                .replace("\"{", "{").replace("}\"", "}"));
+        logData.clear();
 
-    @GetMapping("/internalSpan")
-    public void internalSpan() {
-        // Get current context from initial observation
-        var context = microTracer.currentSpan().context();
-        log.info("FIRST TRACER(traceId: {}, spanId: {}, parentId: {})", context.traceId(), context.spanId(), context.parentId());
-        Observation internalObs = Observation.start("internalObservation", observationRegistry);
-        try (Observation.Scope scope = internalObs.openScope()) {
-            // Get context from internalObservation
-            context = microTracer.currentSpan().context();
-            log.info("INTERNAL TRACER(traceId: {}, spanId: {}, parentId: {})", context.traceId(), context.spanId(), context.parentId());
+        if (internalSpan) {
+            Observation internalObs = Observation.start("internalObservation", observationRegistry);
+            try (Observation.Scope scope = internalObs.openScope()) {
+                // Get context from internalObservation
+                logData.put("control", "INTERNALSPAN");
+                logData.put("MDC", objectMapper.writeValueAsString(MDC.getCopyOfContextMap()));
+                context = microTracer.currentSpan().context();
+                logData.put("tracer", "{ \"traceId\": \"" + context.traceId() + "\", \"spanId\": \"" + context.spanId() + "\", \"parentId\": \"" + context.parentId() + "\"}");
+                log.info(objectMapper.writeValueAsString(logData).replace("\\\"", "\"")
+                        .replace("\"{", "{").replace("}\"", "}"));
+            }
         }
-    }
-
-    @GetMapping("/propagation")
-    public void propagation(@RequestParam(name = "webClient", defaultValue = "false", required = false) boolean withWebClient) {
-        log.info("FIRST ENDPOINT");
-        if (withWebClient) {
-            restTemplate.getForObject("/demo/headers", String.class);
-        } else {
-            webClient.get().uri("/demo/headers").retrieve().bodyToFlux(String.class).blockFirst();
+        if (propagation) {
+            if (url == null) {
+                restTemplate.getForObject("/demo", String.class);
+            } else {
+                restTemplateBuilder.rootUri(url).build().getForObject("/demo", String.class);
+            }
         }
     }
 
